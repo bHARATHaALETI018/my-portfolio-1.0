845493c973d11ac9c8c038f536a9da0ac630f995
Bharath Aaleti <108296519+bHARATHaALETI018@users.noreply.github.com>
2023-08-22 21:41:11 +0530

    Update ContactMe.jsx

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -14,13 +14,13 @@ const ContactMe = () => {
         <div className='flex flex-col space-y-10 mt-20'>
             <h4 className=' md:text-2xl font-semibold text-center p-4'>Don't be shy - I won't bite (unless you're made of chocolate).{" "}<span className='decoration-[#f7ab0a]/50 underline'>Let's Talk</span></h4>
             <div className='space-y-3'>
-                <div className='flex items-center justify-center space-x-3'>
+{/*                 <div className='flex items-center justify-center space-x-3'>
                     <PhoneIcon className='w-7 h-7 text-[#f7ab0a] animate-pulse' />
                     <p className='text-xl'>+918897257030</p>
-                </div>
+                </div> */}
                 <div className='flex items-center justify-center space-x-3'>
                     <EnvelopeIcon className='w-7 h-7 text-[#f7ab0a] animate-pulse' />
-                    <p className='text-xl'>bharathaaleti018@gmail.com</p>
+                    <p className='text-xl'>aaletibharath@gmail.com</p>
                 </div>
                 <div className='flex items-center justify-center space-x-3'>
                     <MapPinIcon className='w-7 h-7 text-[#f7ab0a] animate-pulse' />
